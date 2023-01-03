384aa309db23c079260f912376216ff1366c1f5a
hydroponka <121126395+hydroponka@users.noreply.github.com>
2023-01-03 19:02:42 +0300

    Delete src directory

--- src/main/java/by/Ageenko/learn/task2/service/OrderInputService.java
+++
@@ -1,5 +0,0 @@
-package by.ageenko.learn.task2.service;
-
-public interface OrderInputService {
-     String readData(String filePath);
-}
--- src/main/java/by/Ageenko/learn/task2/service/OrderOutputService.java
+++
@@ -1,8 +0,0 @@
-package by.ageenko.learn.task2.service;
-
-import by.ageenko.learn.task2.entity.Order;
-
-public interface OrderOutputService {
-    String attributeOutput(Order order);
-    void fileWriter(String bill, String file);
-}
